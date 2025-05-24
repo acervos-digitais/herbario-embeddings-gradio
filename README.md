a054ee1b878f8de01a50afc3bfdeadc255623bb9
Thiago Hersan <thiago.hersan+github@gmail.com>
2025-05-23 21:25:31 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+---
+title: Herbário Embeddings by SigLIP2
+emoji: 🌴🛏️
+colorFrom: blue
+colorTo: pink
+sdk: gradio
+sdk_version: 5.0.2
+app_file: app.py
+pinned: false
+---
---
+++ app.py
@@ -0,0 +1,27 @@
+import gradio as gr
+import torch
+
+from transformers import AutoModel, AutoProcessor
+
+MODEL_NAME = "google/siglip2-giant-opt-patch16-256"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+processor = AutoProcessor.from_pretrained(MODEL_NAME)
+model = AutoModel.from_pretrained(MODEL_NAME, device_map="auto").to(DEVICE)
+
+def get_embeddings(img):
+  input = processor(images=img, return_tensors="pt").to(DEVICE)
+  with torch.no_grad():
+    my_embedding = model.get_image_features(**input).detach().squeeze()
+  return my_embedding
+
+with gr.Blocks() as demo:
+  gr.Interface(
+    fn=get_embeddings,
+    inputs="image",
+    outputs="json",
+    flagging_mode="never",
+  )
+
+if __name__ == "__main__":
+   demo.launch()
